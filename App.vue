8e90a84ee849b7276ad93ce4ee62200739211784
hayden <alterhi99@gmail.com>
2021-05-13 19:44:02 +0100

    ver 0.01

---
+++ App.vue
@@ -0,0 +1,83 @@
+<template><!-- eslint-disable max-len -->
+<v-app id="inspire">
+  <v-navigation-drawer color="#1A1A1A" fixed v-model="drawer" permanent :mini-variant.sync="drawer" app>
+    <v-btn absolute right fab @click="drawer = !drawer" :style="{color:white,top: '50%', transform:'translate(75%, -50%)'}">
+      <v-icon v-if="drawer">mdi-chevron-right</v-icon>
+      <v-icon v-else>mdi-chevron-left</v-icon>
+    </v-btn>
+    <v-list dense>
+      <!-- vnav images-->
+      <v-flex align-self-center="true" class="center">
+        <v-img :src="logo" max-height="150" max-width="150" class="center"></v-img>
+      </v-flex>
+      <br>
+      <router-link v-bind:to="{ name: 'Home' }" class="side_bar_link">
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon style="color:white">home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content id="imp" style="color:white">Dashboard</v-list-item-content>
+        </v-list-item>
+      </router-link>
+      <router-link v-bind:to="{ name: 'Contact' }" class="side_bar_link">
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon style="color:white">mdi-briefcase</v-icon>
+          </v-list-item-action>
+          <v-hover v-slot="{ hover }">
+            <v-list-item-content id="imp" style="color:white">Job offer</v-list-item-content>
+          </v-hover>
+        </v-list-item>
+      </router-link>
+      <router-link v-bind:to="{ name: 'Contact' }" class="side_bar_link">
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon style="color:white">mdi-cogs</v-icon>
+          </v-list-item-action>
+          <v-list-item-content id="imp" style="color:white">Upgrade</v-list-item-content>
+        </v-list-item>
+      </router-link>
+      <router-link v-bind:to="{ name: 'Contact' }" class="side_bar_link">
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon style="color:white">mdi-account-circle</v-icon>
+          </v-list-item-action>
+          <v-list-item-content id="imp" style="color:white">Profile</v-list-item-content>
+        </v-list-item>
+      </router-link>
+    </v-list>
+  </v-navigation-drawer>
+  <v-app-bar color="#FFC600" dark fixed app>
+    <v-app-bar-nav-icon style="color:#1A1A1A" @click.stop="drawer = !drawer">
+    </v-app-bar-nav-icon>
+    <v-toolbar-title style="color:#1A1A1A">BRUH</v-toolbar-title>
+    <v-avatar id="avatar" color="#1A1A1A" size="56"></v-avatar>
+  </v-app-bar>
+  <v-main>
+    <v-container fluid>
+      <div id="app">
+        <router-view />
+      </div>
+    </v-container>
+  </v-main>
+  <v-footer color="#FFC600" app>
+    <span style="color:#1A1A1A">&copy;Job Hunter 2021</span>
+  </v-footer>
+</v-app>
+</template>
+
+<script>
+import './assets/stylesheets/main.css';
+import foot1 from './assets/logonav.png';
+
+// eslint-disable-next-line
+export default { //import { mdiCogs } from '@mdi/js';
+  data: () => ({
+    drawer: null,
+    logo: foot1,
+  }),
+  props: {
+    source: String,
+  },
+};
+</script>
---
+++ Home.vue
@@ -0,0 +1,26 @@
+<template>
+<v-layout>
+  <v-layout row wrap>
+    <v-flex xs12>
+      <div v-text="message"></div>
+    </v-flex>
+    <v-flex xs12>
+      <v-btn v-on:click="reply">Reply</v-btn>
+    </v-flex>
+  </v-layout>
+</v-layout>
+</template>
+<script type="text/javascript">
+export default {
+  data() {
+    return {
+      message: 'Hello there, how are you this morning?',
+    };
+  },
+  methods: {
+    reply() /* eslint-disable */ {
+      this.message = "I'm doing great. Thank You!";
+    },
+  },
+};
+</script>
